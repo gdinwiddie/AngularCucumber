f185871848feb0f50856f631db4cd0722cf95bf3
George Dinwiddie <gdinwiddie@idiacomputing.com>
2017-08-23 15:11:50 -0400

    Remove obsolete elements of app.component (now in zip-form.component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,4 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
   title = 'ZIP Code Lookup';
-  private _zipcode = '12345';
-  public dropdownValue: string = '';
-  public foo: string = 'bar';
-
-  @Input()
-  set zipcode(value: string) {
-    this._zipcode = (value && value.trim()) || '<not set>';
-  }
-  get zipcode(): string {
-    return this._zipcode;
-  }
-
-  ngDoCheck() {
-    this.zipcode = '';
-    this.foo = 'baz';
-  }
-
-  onSubmit(value) {
-    alert(`Submit: ${JSON.stringify(value)}`);
-  }
 }
