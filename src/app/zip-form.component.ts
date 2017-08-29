f0f7566d10cc22ea5e94fb75b96c23713f26c210
George Dinwiddie <gdinwiddie@idiacomputing.com>
2017-08-29 12:19:41 -0400

    Created fledgling ZipLookupService

--- src/app/zip-form.component.ts
+++ src/app/zip-form.component.ts
@@ -1,17 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { ZipLookupService } from './zip.service';
 
 @Component({
   selector: 'zip-form',
   templateUrl: './zip-form.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ ZipLookupService ]
 })
 export class ZipFormComponent {
+  constructor(private zipLookupService: ZipLookupService) {
+  }
   zipcode = '';
 
   model = this;
 
   // TODO: this should call the zipcode lookup service
   isValidZip() {
-    return this.zipcode != '00000';
+    return this.zipLookupService.lookup(this.zipcode);
   }
 }
